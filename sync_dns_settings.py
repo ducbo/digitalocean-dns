412395d0d86e08cfc049ff3925b34cc7a09fb15d
Tran Dinh Duc <ducbo@yahoo.com>
2020-11-02 12:40:43 +0700

    Create sync_dns_settings.py

---
+++ sync_dns_settings.py
@@ -0,0 +1,8 @@
+# Example file for sync_dns
+# Copy this file to "sync_dns_settings.py" and adjust with your values.
+# It's in .gitignore so won't be synced
+
+ip="1.1.1.1"
+auth_token=""
+bindfolder = "/root/digitalocean-dns-sync/"
+bindextension = ".zone"
